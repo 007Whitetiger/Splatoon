6261a1551d0ec595de3f71925a6ffa7ae1833453
007Whitetiger <stijntebaerts@gmail.com>
2020-07-29 20:53:20 +0200

    Initial commit

---
+++ src/main/java/me/whitetiger/splatoon/Splatoon.java
@@ -0,0 +1,17 @@
+package me.whitetiger.splatoon;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Splatoon extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
